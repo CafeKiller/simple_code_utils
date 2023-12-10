5d0c81755a56d2eda29b82ba423d3ffa3b6851ba
CoffeeKiller <18320210361@163.com>
2023-12-10 14:34:08 +0800

    feat: golang 获取用户真实ip的方法

---
+++ go/util.go
@@ -0,0 +1,55 @@
+// var r *http.Request
+ip := exnet.ClientPublicIP(r)
+if ip == ""{
+  ip = exnet.ClientIP(r)
+}
+
+// ClientPublicIP 尽最大努力实现获取客户端公网 IP 的算法。
+// 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作。
+func ClientPublicIP(r *http.Request) string {
+	var ip string
+	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
+		ip = strings.TrimSpace(ip)
+		if ip != "" && !HasLocalIPddr(ip) {
+			return ip
+		}
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" && !HasLocalIPddr(ip) {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		if !HasLocalIPddr(ip) {
+			return ip
+		}
+	}
+
+	return ""
+}
+
+// HasLocalIPddr 检测 IP 地址字符串是否是内网地址
+func HasLocalIPddr(ip string) bool {
+	return HasLocalIP(net.ParseIP(ip))
+}
+
+// HasLocalIP 检测 IP 地址是否是内网地址
+func HasLocalIP(ip net.IP) bool {
+	for _, network := range localNetworks {
+		if network.Contains(ip) {
+			return true
+		}
+	}
+
+	return ip.IsLoopback()
+}
+
+// RemoteIP 通过 RemoteAddr 获取 IP 地址， 只是一个快速解析方法。
+func RemoteIP(r *http.Request) string {
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+
+	return ""
+}
